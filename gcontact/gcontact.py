879983485f4a9b01842740ac5bee8f23c35b05cd
Suhas More <suhas.more@joshsoftware.com>
2020-02-08 14:30:12 +0530

    added contact service

---
+++ gcontact/gcontact.py
@@ -0,0 +1,9 @@
+import atom
+import gdata.data
+import gdata.contacts.client
+import gdata.contacts.data
+
+class GContact:
+  def __init__():
+    print("initializing Gmail Contact Service")
+    gd_client = gdata.contacts.client.ContactsClient(source='happy-birthday-265010')
